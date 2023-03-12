5c049f0cf278433f94333f31345470570c9446eb
Roland Weisleder <roland.weisleder@googlemail.com>
2023-03-12 18:49:49 +0100

    Add ArchRule for logging

--- src/test/java/org/springframework/samples/petclinic/ArchitectureTests.java
+++ src/test/java/org/springframework/samples/petclinic/ArchitectureTests.java
@@ -1,22 +1,34 @@
 package org.springframework.samples.petclinic;
 
+import com.tngtech.archunit.core.domain.JavaMethod;
+import com.tngtech.archunit.core.domain.JavaMethodCall;
 import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.CompositeArchRule;
+import com.tngtech.archunit.lang.ConditionEvents;
 import com.tngtech.archunit.library.Architectures;
 import jakarta.persistence.Entity;
 import org.jmolecules.archunit.JMoleculesArchitectureRules;
+import org.slf4j.Logger;
 import org.springframework.data.repository.Repository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.assignableTo;
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
 import static com.tngtech.archunit.core.domain.properties.CanBeAnnotated.Predicates.annotatedWith;
+import static com.tngtech.archunit.lang.ConditionEvent.createMessage;
+import static com.tngtech.archunit.lang.SimpleConditionEvent.satisfied;
+import static com.tngtech.archunit.lang.SimpleConditionEvent.violated;
 import static com.tngtech.archunit.lang.conditions.ArchConditions.dependOnClassesThat;
 import static com.tngtech.archunit.lang.conditions.ArchConditions.not;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
 
 @AnalyzeClasses(packagesOf = PetClinicApplication.class, importOptions = DoNotIncludeTests.class)
 class ArchitectureTests {
@@ -52,5 +64,29 @@ class ArchitectureTests {
 
 	@ArchTest
 	public static final ArchRule JMOLECULES_LAYERS = JMoleculesArchitectureRules.ensureLayering();
+
+	@ArchTest
+	public static final ArchRule CONTROLLER_SHOULD_LOG = methods()
+		.that().areAnnotatedWith(PostMapping.class)
+		.should(log());
+
+	private static ArchCondition<? super JavaMethod> log() {
+		return new ArchCondition<>("log") {
+			@Override
+			public void check(JavaMethod item, ConditionEvents events) {
+				List<JavaMethodCall> loggingCalls = item.getMethodCallsFromSelf().stream()
+					.filter(call -> call.getTargetOwner().isEquivalentTo(Logger.class))
+					.toList();
+
+				if (loggingCalls.isEmpty()) {
+					events.add(violated(item, createMessage(item, "does not log")));
+				} else {
+					for (JavaMethodCall loggingCall : loggingCalls) {
+						events.add(satisfied(item, loggingCall.getDescription()));
+					}
+				}
+			}
+		};
+	}
 
 }

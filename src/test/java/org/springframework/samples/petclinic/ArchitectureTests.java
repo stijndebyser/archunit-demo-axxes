da463ee1efe908a54202b284287d881322e50852
Roland Weisleder <roland.weisleder@googlemail.com>
2023-03-12 17:41:23 +0100

    Add ArchRule for naming

--- src/test/java/org/springframework/samples/petclinic/ArchitectureTests.java
+++ src/test/java/org/springframework/samples/petclinic/ArchitectureTests.java
@@ -2,8 +2,21 @@ package org.springframework.samples.petclinic;
 
 import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
 import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.springframework.stereotype.Controller;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 @AnalyzeClasses(packagesOf = PetClinicApplication.class, importOptions = DoNotIncludeTests.class)
 class ArchitectureTests {
+
+	@ArchTest
+	public static final ArchRule CONTROLLER_NAMING = classes()
+		.that().areAnnotatedWith(Controller.class)
+		.or().haveSimpleNameEndingWith("Controller")
+		.should().beAnnotatedWith(Controller.class)
+		.andShould().haveSimpleNameEndingWith("Controller")
+		.because("controller should be easy to find");
 
 }

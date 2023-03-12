bc3cd5b5c10eea67464ebf03a4c6917db9851fa9
Roland Weisleder <roland.weisleder@googlemail.com>
2023-03-12 18:05:56 +0100

    Add ArchRule for dependencies between modules

--- src/test/java/org/springframework/samples/petclinic/ArchitectureTests.java
+++ src/test/java/org/springframework/samples/petclinic/ArchitectureTests.java
@@ -4,8 +4,12 @@ import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.CompositeArchRule;
 import org.springframework.stereotype.Controller;
 
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
+import static com.tngtech.archunit.lang.conditions.ArchConditions.dependOnClassesThat;
+import static com.tngtech.archunit.lang.conditions.ArchConditions.not;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 @AnalyzeClasses(packagesOf = PetClinicApplication.class, importOptions = DoNotIncludeTests.class)
@@ -18,5 +22,16 @@ class ArchitectureTests {
 		.should().beAnnotatedWith(Controller.class)
 		.andShould().haveSimpleNameEndingWith("Controller")
 		.because("controller should be easy to find");
+
+	@ArchTest
+	public static final ArchRule DEPENDENCIES_BETWEEN_MODULES = CompositeArchRule
+		.of(
+			classes()
+				.that().resideInAPackage("..owner..")
+				.should(not(dependOnClassesThat(resideInAPackage("..vet..")))))
+		.and(
+			classes()
+				.that().resideInAPackage("..vet..")
+				.should(not(dependOnClassesThat(resideInAPackage("..owner..")))));
 
 }
